71a48a4a6cd307196645099f5181e9b9f47380b0
milletbig <33082048+milletbig@users.noreply.github.com>
2022-09-20 16:30:58 +0800

    Update xray_mod1_tls1_3.sh

--- xray_mod1_tls1_3.sh
+++ xray_mod1_tls1_3.sh
@@ -546,7 +546,7 @@ getCert() {
             systemctl start cron
             systemctl enable cron
         fi
-        curl -sL https://get.acme.sh | sh -s email=hiwongshawn@gmail.com
+        curl -sL https://get.acme.sh | sh
         source ~/.bashrc
         ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
         ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
